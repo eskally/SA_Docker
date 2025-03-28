a71609b4e9647fb5cb37365e2c93dbb0d3843092
eskally <ela.skalic@student.um.si>
2025-03-28 16:59:56 +0100

    Implementirano periodično zajemanje slik z nitmi

--- server/server.py
+++ server/server.py
@@ -1,33 +1,41 @@
 from flask import Flask, jsonify
 import cv2
 import base64
+import time
+from threading import Thread
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0) 
+camera = cv2.VideoCapture(0)  
 if not camera.isOpened():
     print("Ne morem odpreti kamere!")
     exit()
 
+image_data = None
+
 def capture_and_encode():
-    ret, frame = camera.read()
-    if not ret:
-        print("Ne morem zajeti slike!")
-        return None
+    global image_data
+    while True:
+        ret, frame = camera.read()
+        if not ret:
+            print("Ne morem zajeti slike!")
+            break
 
-    ret, buffer = cv2.imencode('.jpg', frame)
-    if not ret:
-        print("Ne morem kodirati slike!")
-        return None
+        ret, buffer = cv2.imencode('.jpg', frame)
+        if not ret:
+            print("Ne morem kodirati slike!")
+            break
 
-    img_str = base64.b64encode(buffer).decode('utf-8')
-    return img_str
+        image_data = base64.b64encode(buffer).decode('utf-8')
+        time.sleep(10)  # Zajemi sliko vsakih 10 sekund
+
+    camera.release()
 
 @app.route('/image')
 def image():
-    img_data = capture_and_encode()
-    if img_data:
-        return jsonify({'image': img_data})
+    global image_data
+    if image_data:
+        return jsonify({'image': image_data})
     else:
         return "Ni slike na voljo", 500
 
@@ -39,4 +47,9 @@ def is_camera_working():
         return jsonify({'status': 'ERROR'})
 
 if __name__ == '__main__':
+    # Zaženi zajemanje slik v ločeni niti
+    capture_thread = Thread(target=capture_and_encode)
+    capture_thread.daemon = True  # Zapri nit, ko se aplikacija zapre
+    capture_thread.start()
+
     app.run(debug=True, host='0.0.0.0', port=5001)
