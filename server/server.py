f4b683d384946b6f6c75063ab78195065fc369a0
eskally <ela.skalic@student.um.si>
2025-03-28 16:59:12 +0100

    implementacija zajemanja in kodiranja slike

--- server/server.py
+++ server/server.py
@@ -1,16 +1,35 @@
 from flask import Flask, jsonify
 import cv2
+import base64
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)  # Poskusi 0, 1, itd.
+camera = cv2.VideoCapture(0) 
 if not camera.isOpened():
     print("Ne morem odpreti kamere!")
     exit()
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+def capture_and_encode():
+    ret, frame = camera.read()
+    if not ret:
+        print("Ne morem zajeti slike!")
+        return None
+
+    ret, buffer = cv2.imencode('.jpg', frame)
+    if not ret:
+        print("Ne morem kodirati slike!")
+        return None
+
+    img_str = base64.b64encode(buffer).decode('utf-8')
+    return img_str
+
+@app.route('/image')
+def image():
+    img_data = capture_and_encode()
+    if img_data:
+        return jsonify({'image': img_data})
+    else:
+        return "Ni slike na voljo", 500
 
 @app.route('/is_camera_working')
 def is_camera_working():
